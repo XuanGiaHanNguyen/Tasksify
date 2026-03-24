555e5be26a5bdcb07188fa032ff026ef34f543d8
XuanGiaHanNguyen <han316@usf.edu>
2026-03-24 13:16:16 -0400

    added login

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,21 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'My Tasks',
+  description: 'Oh well well',
   generator: 'v0.app',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logo.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logo.jpg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logo.jpg',
         type: 'image/svg+xml',
       },
     ],
